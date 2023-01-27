96e566946970410a30c60a43136d8c489547cec6
Otim Brian <otims.brian@gmail.com>
2023-01-27 14:01:13 +0300

    Adjusted main.go

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func main() {
+	password := ""
 	m := gomail.NewMessage()
 
 	message.ReciepientAndUser(m, "otims.brian@gmail.com", "brianotims56@gmail.com")
 
 	message.CreateMessageBody(m, "Hello Brian", "Hello Message to you my friend")
 
-	message.SendEmail(m, "otims.brian@gmail.com")
+	message.SendEmail(m, "otims.brian@gmail.com", password)
 
 }
